5d1f38493f436abe352a5a933906d7df5fc69be5
Rae707 <rachiekiwiart@gmail.com>
2025-04-25 14:20:24 -0400

    Boundry

--- Assets/MapTransation.cs
+++ Assets/MapTransation.cs
@@ -1,17 +1,25 @@
 using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class MapTransation : MonoBehaviour
 {
     [SerializeField] PolygonCollider2D mapBoundry;
     CinemachineConfiner confiner;
+    [SerializeField] Direction direction;
+   
+
+    enum Direction {  Up, Down, Left, Right }
     
         private void Awake()
     {
         confiner = FindAnyObjectByType<CinemachineConfiner>();
     }
+    
+     
+   
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -20,4 +28,13 @@ public class MapTransation : MonoBehaviour
             confiner.m_BoundingShape2D = mapBoundry;
         }
     }
+
+
+
+
+
+
+
+
+
 }
